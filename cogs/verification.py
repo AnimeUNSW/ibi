560bd3cbe578b2f220f735ad7813550f2f983351
Andy <andy.ruan2005@gmail.com>
2025-03-16 14:01:42 +1100

    add user data into the database

--- cogs/verification.py
+++ cogs/verification.py
@@ -15,8 +15,23 @@ class VerifyModal(discord.ui.Modal):
 	id = discord.ui.TextInput(label="zID:", placeholder="Enter your ZID")
 	
 	async def on_submit(self, interaction: discord.interaction):
-		pass
-		# send data to database and idk maybe send user a DM.
+		async with self.bot.db.connection() as conn:
+			async with conn.cursor() as cur:
+				await conn.execute(
+					"INSERT INTO verification_data (user_id, name, username, email, zid) VALUES (%s, %s, %s, %s, %s)",
+					(
+						interaction.user.id,
+						self.name.value,
+						self.username.value,
+						self.email.value,
+						self.id.value,
+					),
+				)
+            	await conn.commit()
+
+        await interaction.response.send_message(
+            "Love letter submitted!", ephemeral=True
+        )
 
 
 class Verification(commands.Cog):
