08f8a717c80ded544d9b4b1a12b1046632f1662b
stickie <71910589+FieryIceStickie@users.noreply.github.com>
2025-04-26 01:35:46 +1000

    make it actually print exceptions to console instead of silently erroring

--- utils/tree.py
+++ utils/tree.py
@@ -1,3 +1,5 @@
+import traceback
+
 from discord.app_commands import CommandTree
 from discord import Interaction
 
@@ -5,3 +7,6 @@ from discord import Interaction
 class Tree(CommandTree):
     async def interaction_check(self, interaction: Interaction) -> bool:
         return True
+
+    async def on_error(self, interaction: Interaction, error: Exception) -> None:
+        traceback.print_exception(error)
